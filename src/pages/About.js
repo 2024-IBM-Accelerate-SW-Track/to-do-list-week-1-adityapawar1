54b317f82027f39390bbf0ec08c0e5c2fe99352f
Aditya Pawar <apawar@berkeley.edu>
2024-06-05 16:42:15 -0700

    Finish todo list component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
-  
+import profile_picture from "../assets/pfp.png";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                height={42}
+                width={42}
+                src={profile_picture}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Aditya Pawar</div>
+              <div className="brief_description">
+                <ul>
+                  <li>19 years old</li>
+                  <li>EECS student as UC Berkeley</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
